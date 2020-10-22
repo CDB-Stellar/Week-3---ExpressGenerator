c993719313d705aad6d800348a14f3d12bf67e7e
Chloe <chloe_baker@live.com>
2020-10-22 12:17:19 -0400

    Week 6.4
    - made the book_store database online
    - updated URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/book_store" //connect to the local book_store database
+    //old URI because now it will connect to online database.
+    //"URI": "mongodb://localhost/book_store" //connect to the local book_store database
+    "URI": "mongodb+srv://chloe:rn3So5L2I77YhvrV@mongodbserver.vpx2b.mongodb.net/book_store?retryWrites=true&w=majority"
 }
